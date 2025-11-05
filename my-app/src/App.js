a7e4e4c65f64c5e466b41b94629a41e895f64936
sadio-kanoute <sadio.kanoute@laplateforme.io>
2025-11-05 11:00:06 +0100

    mise en place pour commencer le projet

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div 
+        >
+         
+    </div>
+  );
+}
+
+export default App;
+// composant carte button titre etc et reutiliser 
+// importer les images et les elements de react comme les hooks
+
